e048f11db2be9a93ceeaced08bc92935f26c9e4f
Andreas Boss <andi@theboss.ch>
2019-05-12 17:41:54 +0200

    Fix subject not available on loading

--- src/app/mowers/mower-detail/mower-detail.component.ts
+++ src/app/mowers/mower-detail/mower-detail.component.ts
@@ -1,30 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-
-import { MowerStatus } from '../mower.model';
 import { MowerService } from '../mower.service';
-import { Observable, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-mower-detail',
   templateUrl: './mower-detail.component.html',
   styleUrls: ['./mower-detail.component.css']
 })
-export class MowerDetailComponent implements OnInit {
-  mowerStatus: BehaviorSubject<MowerStatus>;
+export class MowerDetailComponent {
+  mowerStatus = this.mowerService.mowerStatus;
   id: string;
 
   constructor(
     private mowerService: MowerService,
     private route: ActivatedRoute
-  ) {}
-
-  ngOnInit() {
-    this.mowerStatus = this.mowerService.mowerStatus;
+  ) {
     this.route.params.subscribe((params: Params) => {
       this.id = params.id;
-      console.log('ID: ', this.id);
-
       this.mowerService.loadMowerStatus(this.id);
     });
   }
